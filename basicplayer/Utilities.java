548865737e0b4efa142e059136e86bf7300eca52
JonathanYang0127 <jydumplings@gmail.com>
2018-01-10 13:19:08 -0800

    Add files via upload
    Utilities contains useful functions such as BFS (for width of area), DFS, etc.

---
+++ basicplayer/Utilities.java
@@ -0,0 +1,16 @@
+import bc.Direction;
+import bc.GameController;
+import bc.Unit;
+
+public class Utilities {
+    private static Direction
+    
+    public Utilities(int length, int width) {
+    	
+    }
+    
+    public static void astar() {
+    		
+    }
+
+}
