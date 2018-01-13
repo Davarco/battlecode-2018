69646a08ca47bb38bc593ee7becfd0434aee8948
daniellionyang <daniellionyang@gmail.com>
2018-01-12 23:37:52 -0800

    Add files via upload

---
+++ basicplayer/TeamUtil.java
@@ -0,0 +1,30 @@
+import bc.*;
+
+import java.util.HashMap;
+
+public class TeamUtil {
+
+    private static GameController gc;
+    private static Team friendly;
+    private static Team enemy;
+
+    public static void init(GameController controller) {
+        gc = controller;
+
+        // Initialize our team and enemy team
+        friendly = gc.team();
+        enemy = (friendly.equals(Team.Red)) ? Team.Blue : Team.Red;
+    }
+
+    public static Team friendlyTeam() {
+        return friendly;
+    }
+
+    public static Team enemyTeam() {
+        return enemy;
+    }
+
+    public static boolean friendlyUnit(Unit unit) {
+        return unit.team().equals(friendlyTeam());
+    }
+}
