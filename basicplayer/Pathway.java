a888384f2dd172ac6229da6cb92af840b03b8cee
daniellionyang <daniellionyang@gmail.com>
2018-01-13 20:09:23 -0800

    Add files via upload

---
+++ basicplayer/Pathway.java
@@ -0,0 +1,38 @@
+import bc.*;
+import java.util.*;
+
+public class Pathway {
+	public ArrayList<MapLocation> unitpathway;
+	public int index;
+	public MapLocation goal;
+	
+	public Pathway(ArrayList<MapLocation> p, MapLocation g) {
+		unitpathway = p;
+		index = 0;
+		goal = g;
+		/*if(unitpathway == null) {
+			System.out.println("HI");
+		}
+		for(MapLocation sadf:unitpathway) {
+			System.out.println(sadf.getX()+", "+sadf.getY()+", ");
+		}*/
+	}
+	
+	public MapLocation getNextLocation() {
+		return unitpathway.get(index);
+	}
+	
+	public void setNewPathway(ArrayList<MapLocation> p, MapLocation k) {
+		unitpathway = p;
+		goal = k;
+		index = 0;
+	}
+	
+	public boolean NextLocationIsEnd() {
+		return index == unitpathway.size()-1;
+	}
+	
+	public boolean PathwayDoesNotExist() {
+		return unitpathway == null;
+	}
+}
