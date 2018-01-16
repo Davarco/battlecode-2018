0c5bf5500d9e66a984bd62aa993beaf8e9d1ce80
Timothy Kanarsky <tkanarsky@outlook.com>
2018-01-15 21:28:13 -0800

    Fixed teamutil -> util

--- basicplayer/Pathing.java
+++ basicplayer/Pathing.java
@@ -3,20 +3,37 @@ import bc.*;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.LinkedList;
+import java.util.PriorityQueue;
 
 public class Pathing {
 
     // Movements correspond from N -> NE... -> W -> SW.
-    public static int move[][] = {
+    private static int move[][] = {
             {0, 1}, {1, 1}, {1, 0}, {1, -1},
             {0, -1}, {-1, -1}, {-1, 0}, {-1, 1}
     };
-
     private static GameController gc;
     private static PlanetMap map;
     private static int H, W;
-    private static int prev[][];
-    private static boolean visited[][];
+    public static final int DIAGONAL_COST = 10;
+    public static final int V_H_COST = 10;
+
+    static class Cell{
+        int heuristicCost = 0; //Heuristic cost
+        int finalCost = 0; //G+H
+        int i, j;
+        Cell parent;
+
+        Cell(int i, int j){
+            this.i = i;
+            this.j = j;
+        }
+
+        @Override
+        public String toString(){
+            return "["+this.i+", "+this.j+"]";
+        }
+    }
 
     public static void init(GameController controller) {
 
@@ -29,90 +46,155 @@ public class Pathing {
         W = (int)map.getWidth();
         H = (int)map.getHeight();
     }
+    static Cell [][] grid = new Cell[5][5];
 
-    /*
-    Runs BFS to get the right direction for the robot to move from point A to B.
-     */
-    private static ArrayList<MapLocation> path(Unit unit, MapLocation start, MapLocation end) {
+    static PriorityQueue<Cell> open;
 
-        // Initialize direction grid
-        // System.out.println("Running pathing! " + start + " to " + end);
-        Planet planet = start.getPlanet();
-        prev = new int[W][H];
-        int x=start.getX(), y=start.getY();
+    static boolean closed[][];
+    static int startI, startJ;
+    static int endI, endJ;
 
-        // Run BFS from start node
-        LinkedList<MapLocation> queue = new LinkedList<>();
-        boolean[][] visited = new boolean[W][H];
-        for(int x1 = 0; x1<W; x1++) {
-        	 for(int y1 = 0; y1<H; y1++) {
-         		visited[x1][y1] = false;
-             }
-        }
-        visited[x][y] = true;
-        queue.add(new MapLocation(planet, x, y));
-        System.out.println("Moving Troop Number"+unit.id()+"("+start.getX()+", "+start.getY()+") to ("+end.getX()+", "+end.getY()+")");
-        // Run until queue is empty
-        boolean found = false;
-        boolean ReachedEnd = false;
-        while (!queue.isEmpty() && ReachedEnd == false) {
-            MapLocation location = queue.poll();
-            if(location.getX() == end.getX() && location.getY() == end.getY()) {
-            	 	ReachedEnd  = true;
-            	 	break;
+
+    public static void setStartCell(int i, int j){
+        startI = i;
+        startJ = j;
+    }
+
+    public static void setEndCell(int i, int j){
+        endI = i;
+        endJ = j;
+    }
+
+    static void checkAndUpdateCost(Cell current, Cell t, int cost, MapLocation temp, Unit unit, MapLocation start){
+        if (map.onMap(temp) && map.isPassableTerrainAt(temp) == 1 && !closed[t.i][t.j] && (!temp.isWithinRange(unit.visionRange(), start) || !gc.hasUnitAtLocation(temp)) ) {
+            int t_final_cost = t.heuristicCost+cost;
+
+            boolean inOpen = open.contains(t);
+            if(!inOpen || t_final_cost<t.finalCost){
+                t.finalCost = t_final_cost;
+                t.parent = current;
+                if(!inOpen)open.add(t);
             }
-            for (int i = 0; i < 8; i++) {
-                int a = location.getX() + move[i][0];
-                int b = location.getY() + move[i][1];
-                MapLocation temp = new MapLocation(planet, a, b);
-                if (map.onMap(temp) && map.isPassableTerrainAt(temp) == 1 && !visited[a][b]) {
-                		// && (!temp.isWithinRange(unit.visionRange(), start) || !gc.hasUnitAtLocation(temp)) &&
-                		prev[a][b] = i;
-                    // System.out.println(prev[a][b] + " " + a + " " + b);
-                    visited[a][b] = true;
-                    queue.add(new MapLocation(planet, a, b));
+        }
+    }
+    public static void AStar(MapLocation start, MapLocation end,Unit unit){
+        //add the start location to open list.
+        open.add(grid[startI][startJ]);
+
+        Cell current;
+        Planet planet = start.getPlanet();
+        while(true){
+            current = open.poll();
+            if(current==null) break;
+            closed[current.i][current.j]=true;
+
+            if(current.equals(grid[endI][endJ])){
+                return;
+            }
+
+            Cell t;
+            MapLocation temp;
+            if(current.i-1>=0){
+                temp = new MapLocation(planet, current.i-1, current.j);
+                t = grid[current.i-1][current.j];
+                checkAndUpdateCost(current, t, current.finalCost+V_H_COST,temp,unit,start);
+
+                if(current.j-1>=0){
+                    temp = new MapLocation(planet, current.i-1, current.j-1);
+                    t = grid[current.i-1][current.j-1];
+                    checkAndUpdateCost(current, t, current.finalCost+DIAGONAL_COST,temp,unit,start);
+                }
+
+                if(current.j+1<grid[0].length){
+                    temp = new MapLocation(planet, current.i-1, current.j+1);
+                    t = grid[current.i-1][current.j+1];
+                    checkAndUpdateCost(current, t, current.finalCost+DIAGONAL_COST,temp,unit,start);
+                }
+            }
+
+            if(current.j-1>=0){
+                temp = new MapLocation(planet, current.i, current.j-1);
+                t = grid[current.i][current.j-1];
+                checkAndUpdateCost(current, t, current.finalCost+V_H_COST,temp,unit,start);
+            }
+
+            if(current.j+1<grid[0].length){
+                temp = new MapLocation(planet, current.i, current.j+1);
+                t = grid[current.i][current.j+1];
+                checkAndUpdateCost(current, t, current.finalCost+V_H_COST,temp,unit,start);
+            }
+
+            if(current.i+1<grid.length){
+                temp = new MapLocation(planet, current.i+1, current.j);
+                t = grid[current.i+1][current.j];
+                checkAndUpdateCost(current, t, current.finalCost+V_H_COST,temp,unit,start);
+
+                if(current.j-1>=0){
+                    temp = new MapLocation(planet, current.i+1, current.j-1);
+                    t = grid[current.i+1][current.j-1];
+                    checkAndUpdateCost(current, t, current.finalCost+DIAGONAL_COST,temp,unit,start);
+                }
+
+                if(current.j+1<grid[0].length){
+                    temp = new MapLocation(planet, current.i+1, current.j+1);
+                    t = grid[current.i+1][current.j+1];
+                    checkAndUpdateCost(current, t, current.finalCost+DIAGONAL_COST,temp,unit,start);
                 }
             }
         }
-        System.out.println(ReachedEnd);
-        /*
-        Use this to debug.
-        for (int i = H-1; i > 0; i--) {
-            for (int j = 0; j < W; j++) {
-                System.out.print(prev[j][i]);
-            }
-            System.out.println();
-        }
-        */
-
-        // Go backwards from end point
-        if(!ReachedEnd) {
-        		return null;
-        }
-        MapLocation lastLoc = end.clone();
-        ArrayList<MapLocation> ml = new ArrayList<MapLocation>();
-        while (!end.equals(start)) {
-            // Subtract direction, NOT add
-            int a=end.getX(), b=end.getY();
-            lastLoc.setX(a);
-            lastLoc.setY(b);
-          //  System.out.println(end.getX()+" "+end.getY()+" "+prev[end.getX()][end.getY()]);
-            end.setX(a-move[prev[a][b]][0]);
-            end.setY(b-move[prev[a][b]][1]);
-            ml.add(lastLoc.clone());
-            // System.out.println("Subtracting " + prev[a][b] + " from " + lastLoc + " forms " + end);
-        }
-        Collections.reverse(ml);
-        // System.out.println(dir);
-        return ml.size() == 0?null:ml;
     }
-    
- //        return ml.size() == 0?null:ml;
-//    !gc.hasUnitAtLocation(new MapLocation(planet, a, b)
+    public static ArrayList<MapLocation> path(Unit TroopUnit, MapLocation start, MapLocation end){
+        int x = W;
+        int y = H;
+        //Reset
+        Planet planet = start.getPlanet();
+        int si=start.getX(), sj=start.getY();
+        int ei=end.getX(), ej=end.getY();
 
-    /*
-    Gets the direction opposite of an input direction.
-     */
+        //System.out.println("dkhfjbskhfbdsih:"+ start);
+        grid = new Cell[x][y];
+        closed = new boolean[x][y];
+        open = new PriorityQueue<>((Object o1, Object o2) -> {
+            Cell c1 = (Cell)o1;
+            Cell c2 = (Cell)o2;
+
+            return c1.finalCost<c2.finalCost?-1:
+                    c1.finalCost>c2.finalCost?1:0;
+        });
+        //Set start position
+        setStartCell(si, sj);  //Setting to 0,0 by default. Will be useful for the UI part
+
+        //Set End Location
+        setEndCell(ei, ej);
+
+        //generate heuristics
+        for(int i=0;i<x;++i){
+            for(int j=0;j<y;++j){
+                grid[i][j] = new Cell(i, j);
+                grid[i][j].heuristicCost = Math.abs(i-endI)+Math.abs(j-endJ);
+            }
+        }
+        grid[si][sj].finalCost = 0;
+        AStar(start,end, TroopUnit);
+        ArrayList<MapLocation> ans = new ArrayList<MapLocation>();
+        if(closed[endI][endJ]){
+            //Trace back the path
+            System.out.println("Path: ");
+            Cell current = grid[endI][endJ];
+            System.out.print(current);
+            ans.add(new MapLocation(planet,current.i,current.j));
+            while(current.parent!=null){
+                ans.add(new MapLocation(planet,current.parent.i,current.parent.j));
+                System.out.print(current.parent + " <- ");
+                current = current.parent;
+
+            }
+            Collections.reverse(ans);
+            ans.remove(0);
+            return ans;
+        }else return null;
+    }
+
     private static Direction opposite(Direction direction) {
         switch (direction) {
             case North:
@@ -136,17 +218,18 @@ public class Pathing {
         return null;
     }
 
+
     /*
     Criterion for (re)calculating path:
      	1. If the unit was just made
      	2. If the goal changed
      	3. If unit is blocking the goal
     If recalculated path is null given the update of the situation, then the unit cannot move (function returns)
-     	
+
     First, checks if the unit movement cooldown is up
     Next, checks if the unit is already at the location
     		If it is, then returns (mission accomplished)
-    	
+
     Criterion 1
     Next, checks if the unit has had a pathway
     	Creates one if not
@@ -155,7 +238,7 @@ public class Pathing {
     If goal changed from previous (for units with previous pathways)
     	Recalculate path
     	If recalculation is null, then returns (can't move)
-    			
+
     Criterion 3
     Next, checks if the next location to move is blocked
    	    If blocked and next location is final destination, doesn't recalculate
@@ -163,79 +246,80 @@ public class Pathing {
     	If not, then recalculates path
     		If path is null then return
     Move unit
-    
+
+
     True if move successful, false if not
        */
-    
-    
-    public static boolean move(Unit TroopUnit, MapLocation end) {
-		if(!gc.isMoveReady(TroopUnit.id())) { //check if unit can move
-			return false;
-		}
-		if(TroopUnit.location().mapLocation().equals(end)) { //check if unit is at location
-			return true;
-		}
-		//Critertion 1
-		if(!Player.unitpaths.containsKey(TroopUnit.id())) { 				//check if no previous path array
-			Player.unitpaths.put(TroopUnit.id(), new Pathway(path(TroopUnit, TroopUnit.location().mapLocation(), end.clone()), end.clone()));
-		}
-		if(Player.unitpaths.get(TroopUnit.id()).PathwayDoesNotExist()) {
-			return false;
-		}
-		Pathway TroopPath = Player.unitpaths.get(TroopUnit.id());
 
-		//Criterion 2
-		if(!end.equals(TroopPath.goal)) {
-			Player.unitpaths.get(TroopUnit.id()).setNewPathway(path(TroopUnit, TroopUnit.location().mapLocation(), end.clone()), end.clone());
-			if(Player.unitpaths.get(TroopUnit.id()).PathwayDoesNotExist()) {
-				return false;
-			}
-		}
-		MapLocation next = TroopPath.getNextLocation();
-		//Criterion 3
-		if(!gc.canMove(TroopUnit.id(), TroopUnit.location().mapLocation().directionTo(next))) { 	//check if unit is in the way and unit is not in final location
-			//might need to override later
-			if(TroopPath.NextLocationIsEnd()) {
-				return false;
-			}
-			else {
-				Player.unitpaths.get(TroopUnit.id()).setNewPathway(path(TroopUnit, TroopUnit.location().mapLocation(), end.clone()), end.clone());
-				if(Player.unitpaths.get(TroopUnit.id()).PathwayDoesNotExist()) {
-					return false;
-				}
-			}
-		}
-		
-		//Move unit
-		if(gc.canMove(TroopUnit.id(), TroopUnit.location().mapLocation().directionTo(next))){
-			gc.moveRobot(TroopUnit.id(), TroopUnit.location().mapLocation().directionTo(next));
-			TroopPath.index++; 
-			return true;
-		}
-		return false;
+
+    public static boolean move(Unit TroopUnit, MapLocation end) {
+        if(!gc.isMoveReady(TroopUnit.id())) { //check if unit can move
+            return false;
+        }
+        if(TroopUnit.location().mapLocation().equals(end)) { //check if unit is at location
+            return true;
+        }
+        //Critertion 1
+        if(!Player.unitpaths.containsKey(TroopUnit.id())) { 				//check if no previous path array
+            Player.unitpaths.put(TroopUnit.id(), new Pathway(path(TroopUnit, TroopUnit.location().mapLocation(), end.clone()), end.clone()));
+        }
+        if(Player.unitpaths.get(TroopUnit.id()).PathwayDoesNotExist()) {
+            return false;
+        }
+        Pathway TroopPath = Player.unitpaths.get(TroopUnit.id());
+
+        //Criterion 2
+        if(!end.equals(TroopPath.goal)) {
+            Player.unitpaths.get(TroopUnit.id()).setNewPathway(path(TroopUnit, TroopUnit.location().mapLocation(), end.clone()), end.clone());
+            if(Player.unitpaths.get(TroopUnit.id()).PathwayDoesNotExist()) {
+                return false;
+            }
+        }
+        MapLocation next = TroopPath.getNextLocation();
+        //Criterion 3
+        if(!gc.canMove(TroopUnit.id(), TroopUnit.location().mapLocation().directionTo(next))) { 	//check if unit is in the way and unit is not in final location
+            //might need to override later
+            if(TroopPath.NextLocationIsEnd()) {
+                return false;
+            }
+            else {
+                Player.unitpaths.get(TroopUnit.id()).setNewPathway(path(TroopUnit, TroopUnit.location().mapLocation(), end.clone()), end.clone());
+                if(Player.unitpaths.get(TroopUnit.id()).PathwayDoesNotExist()) {
+                    return false;
+                }
+            }
+        }
+
+        //Move unit
+        if(gc.canMove(TroopUnit.id(), TroopUnit.location().mapLocation().directionTo(next))){
+            gc.moveRobot(TroopUnit.id(), TroopUnit.location().mapLocation().directionTo(next));
+            TroopPath.index++;
+            return true;
+        }
+        return false;
     }
-    
+
     private static MapLocation DirectionToMapLocation(Unit unit, Direction direction) {
-		switch (direction) {
-		case North:
-			return new MapLocation(unit.location().mapLocation().getPlanet(), unit.location().mapLocation().getX(), unit.location().mapLocation().getY()+1);
-		case Northeast:
-			return new MapLocation(unit.location().mapLocation().getPlanet(), unit.location().mapLocation().getX()+1, unit.location().mapLocation().getY()+1);
-		case East:
-			return new MapLocation(unit.location().mapLocation().getPlanet(), unit.location().mapLocation().getX()+1, unit.location().mapLocation().getY());
-		case Southeast:
-			return new MapLocation(unit.location().mapLocation().getPlanet(), unit.location().mapLocation().getX()+1, unit.location().mapLocation().getY()-1);
-		case South:
-			return new MapLocation(unit.location().mapLocation().getPlanet(), unit.location().mapLocation().getX(), unit.location().mapLocation().getY()-1);
-		case Southwest:
-			return new MapLocation(unit.location().mapLocation().getPlanet(), unit.location().mapLocation().getX()-1, unit.location().mapLocation().getY()-1);
-		case West:
-			return new MapLocation(unit.location().mapLocation().getPlanet(), unit.location().mapLocation().getX()-1, unit.location().mapLocation().getY());
-		case Northwest:
-			return new MapLocation(unit.location().mapLocation().getPlanet(), unit.location().mapLocation().getX()-1, unit.location().mapLocation().getY()+1);
-		}
-		return null;
-	}
+        switch (direction) {
+            case North:
+                return new MapLocation(unit.location().mapLocation().getPlanet(), unit.location().mapLocation().getX(), unit.location().mapLocation().getY()+1);
+            case Northeast:
+                return new MapLocation(unit.location().mapLocation().getPlanet(), unit.location().mapLocation().getX()+1, unit.location().mapLocation().getY()+1);
+            case East:
+                return new MapLocation(unit.location().mapLocation().getPlanet(), unit.location().mapLocation().getX()+1, unit.location().mapLocation().getY());
+            case Southeast:
+                return new MapLocation(unit.location().mapLocation().getPlanet(), unit.location().mapLocation().getX()+1, unit.location().mapLocation().getY()-1);
+            case South:
+                return new MapLocation(unit.location().mapLocation().getPlanet(), unit.location().mapLocation().getX(), unit.location().mapLocation().getY()-1);
+            case Southwest:
+                return new MapLocation(unit.location().mapLocation().getPlanet(), unit.location().mapLocation().getX()-1, unit.location().mapLocation().getY()-1);
+            case West:
+                return new MapLocation(unit.location().mapLocation().getPlanet(), unit.location().mapLocation().getX()-1, unit.location().mapLocation().getY());
+            case Northwest:
+                return new MapLocation(unit.location().mapLocation().getPlanet(), unit.location().mapLocation().getX()-1, unit.location().mapLocation().getY()+1);
+        }
+        return null;
+    }
 
     public static void move(Unit unit, Direction direction) {
         move(unit, DirectionToMapLocation(unit, direction));
@@ -281,7 +365,7 @@ public class Pathing {
             System.out.println("Error: " + unit.location().mapLocation() + " is stuck!");
             return;
         }
-        
+
         move(unit, Direction.values()[fin]);
     }
 
@@ -320,3 +404,4 @@ public class Pathing {
         return true;
     }
 }
+
