7f332886aa673fdd31ff2fca56321766624ed26c
daniellionyang <daniellionyang@gmail.com>
2018-01-17 01:01:26 -0800

    Add files via upload

---
+++ basicplayer(merged)/Knight.java
@@ -0,0 +1,27 @@
+import bc.Direction;
+import bc.GameController;
+import bc.Unit;
+
+public class Knight {
+
+    private static Unit knight;
+    private static GameController gc;
+
+    public static void init(GameController controller) {
+        gc = controller;
+    }
+
+    public static void run(Unit unit) {
+
+        // Receive knight from main runner
+        knight = unit;
+        if (knight.location().isInGarrison() || knight.location().isInSpace()) return;
+
+        // Move unit (placeholder for now)
+        for (Direction direction: Direction.values()) {
+            if (gc.isMoveReady(knight.id()) && gc.canMove(knight.id(), direction)) {
+                gc.moveRobot(knight.id(), direction);
+            }
+        }
+    }
+}
