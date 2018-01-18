0245aa8d7dda0ae810af0663aafdda95fad410fb
JonathanYang0127 <jydumplings@gmail.com>
2018-01-17 21:07:30 -0800

    Update Pathway.java

--- dankplayer/Pathway.java
+++ dankplayer/Pathway.java
@@ -5,21 +5,24 @@ public class Pathway {
 	public ArrayList<MapLocation> unitpathway;
 	public int index;
 	public MapLocation goal;
+	public MapLocation start;
 	
-	public Pathway(ArrayList<MapLocation> p, MapLocation g) {
+	public Pathway(ArrayList<MapLocation> p, MapLocation g, MapLocation s) {
 		unitpathway = p;
 		index = 0;
 		goal = g;
+		start = s;
 	}
 	
 	public MapLocation getNextLocation() {
 		return unitpathway.get(index);
 	}
 	
-	public void setNewPathway(ArrayList<MapLocation> p, MapLocation k) {
+	public void setNewPathway(ArrayList<MapLocation> p, MapLocation k, MapLocation s) {
 		unitpathway = p;
 		goal = k;
 		index = 0;
+		start = s;
 	}
 	
 	public boolean NextLocationIsEnd() {
