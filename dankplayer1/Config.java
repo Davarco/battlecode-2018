bb8adb7da6c0a64394692d1ce9af0893b513f06f
Timothy Kanarsky <tkanarsky@outlook.com>
2018-01-18 22:45:20 -0800

    Removed useless config entries

--- dankplayer1/Config.java
+++ dankplayer1/Config.java
@@ -1,7 +1,3 @@
 public class Config {
-    public static final int ROCKET_CREATION_ROUND = 300;
-    public static final int ROCKET_EQUILIBRIUM = 4;
-    public static final int RANGER_AUTO_ATTACK_ROUND = 100;
-    public static final int ROUNDS_TO_DEVOTE_TO_FACTORIES = 30;
     public static final int FACTORY_STANDOFF_RADIUS = 6;
 }
