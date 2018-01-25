ea8227db7671e5ef7c973e06611bcad20e61f2cc
David Zhang <davarco123@gmail.com>
2018-01-25 10:59:03 -0800

    Only use three folders

--- dankplayer/Config.java
+++
@@ -1,8 +0,0 @@
-public class Config {
-    public static final int ROCKET_CREATION_ROUND = 300;
-    public static final int ROCKET_EQUILIBRIUM = 4;
-    public static final int RANGER_AUTO_ATTACK_ROUND = 100;
-    public static final int ROUNDS_TO_DEVOTE_TO_FACTORIES = 30;
-    public static final int FACTORY_EQUILIBRIUM = 5;
-    public static final int WORKER_EQUILIBRIUM = 4;
-}
