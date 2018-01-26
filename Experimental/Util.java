fcde1450a6308338b4308ffee0055d738e22e256
daniellionyang <daniellionyang@gmail.com>
2018-01-26 00:58:43 -0800

    Add files via upload

---
+++ Experimental/Util.java
@@ -0,0 +1,50 @@
+import bc.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Util {
+
+    private static GameController gc;
+    private static Team friendly;
+    private static Team enemy;
+
+    public static void init(GameController controller) {
+        gc = controller;
+
+        // Initialize our team and enemy team
+        friendly = gc.team();
+        enemy = (friendly.equals(Team.Red)) ? Team.Blue : Team.Red;
+    }
+
+    public static Team friendlyTeam() {
+        return friendly;
+    }
+
+    public static Team enemyTeam() {
+        return enemy;
+    }
+
+    public static boolean friendlyUnit(Unit unit) {
+        return unit.team().equals(friendlyTeam());
+    }
+
+    public static Planet oppositePlanet(Planet planet) {
+        if (planet.equals(Planet.Earth)) return Planet.Mars;
+        return Planet.Earth;
+    }
+
+    public static HashMap<MapLocation, Boolean> openSpacesAround(MapLocation m, int maxSpacesAround) {
+        HashMap<MapLocation, Boolean> locs = new HashMap<MapLocation, Boolean>();
+        for (Direction d : Direction.values()) {
+            MapLocation loc = m.add(d);
+            if (gc.startingMap(Planet.Earth).onMap(loc) && (gc.startingMap(Planet.Earth).isPassableTerrainAt(loc) == 1) && (gc.isOccupiable(loc) == 1)) {
+                locs.put(loc, true);
+            }
+        }
+        return locs;
+    }
+
+}
